bef7bc37351804570830d8ef319eb2b848e75350
GeorgeOL <george6472@gmail.com>
2023-04-20 15:57:53 +0300

    20.04.2023

---
+++ es2020.js
@@ -0,0 +1,24 @@
+//bigInt
+//Nullish Coalescing Operator ??
+//Optional Chaining Operator ?.
+//Promise.allSettled
+//globalThis
+
+let will_pokemon = {
+    pikachu: {
+        species: `Mouse`,
+        height: 0.4,
+        weight: 6
+    }
+}
+
+let george_pokemon = {
+    raichu: {
+        species: `Mouse`,
+        height: 0.8,
+        weight: 30
+    }
+}
+
+let weight = will_pokemon.pikachu.weight
+console.log(weight)
